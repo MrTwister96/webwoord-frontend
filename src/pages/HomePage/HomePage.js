ebea402576c3ed544a9f1b434a0e1cef5aa8658e
Schalk <schalkolivier@vegamine.com>
2021-10-05 23:26:12 +0200

    Started With Home Page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -4,8 +4,10 @@ import "./HomePage.css";
 
 const HomePage = () => {
     return (
-        <div>
-            <h1>Home Page</h1>
+        <div className="flex flex-1 items-center justify-around sm:p-5 md:p-10 lg:p-20 min-h-screen bg-gray-900 bg-opacity-95">
+            <h1 className="flex bg-opacity-50 bg-gray-900 p-2 sm:p-5 md:p-10 lg:p-20 rounded-lg text-white border-white border border-opacity-50">
+                <h1>all streams will go here</h1>
+            </h1>
         </div>
     );
 };
