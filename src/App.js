660e2a692908b9dd55a16849c6762eed35058e5e
Schalk <schalkolivier@vegamine.com>
2021-10-05 22:36:00 +0200

    Initial react-router-dom implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// Pages
+import HomePage from "./pages/HomePage/HomePage";
+// Styles
 import "./App.css";
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
+        <Router>
+            <Switch>
+                <Route path="/">
+                    <HomePage />
+                </Route>
+            </Switch>
+        </Router>
     );
 };
 
