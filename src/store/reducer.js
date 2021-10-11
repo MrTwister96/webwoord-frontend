e5ab0a2229b3873bbfcffdbabb9935e8a84160a6
Schalk <schalkolivier@vegamine.com>
2021-10-11 15:16:17 +0200

    Cleared Initial State

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,38 +2,7 @@ import actions from "./actions";
 
 const initialState = {
     identity: "",
-    streams: [
-        {
-            streamer: "EG Kerk Maranata",
-            speaker: "Ds Willie Olivier",
-            description: "Die Nagmal - 1 Kor 11:27",
-            listeners: 39,
-        },
-        {
-            streamer: "EG Kerk Pretoria Oos",
-            speaker: "Ds Andre Bester",
-            description: "Verbly jouself - Fil 4:4",
-            listeners: 27,
-        },
-        {
-            streamer: "EG Kerk Benoni",
-            speaker: "Ds Gerhard Beukes",
-            description: "Die Wonderlike Skepping - Gen 1",
-            listeners: 32,
-        },
-        {
-            streamer: "EG Kerk Kempton Park",
-            speaker: "Ds Paul Grobbelaar",
-            description: "Loof die Here - Ps 102",
-            listeners: 18,
-        },
-        {
-            streamer: "EG Kerk Wonderboom",
-            speaker: "Ds Jurgens Prinsloo",
-            description: "Die Here is my Herder - Ps 23",
-            listeners: 22,
-        },
-    ],
+    streams: [],
 };
 
 const reducer = (state = initialState, action) => {
