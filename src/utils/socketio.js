54a89b8e4bda9c26653ec1a44c22cfdd7fcb6d61
Schalk <schalkolivier@vegamine.com>
2021-10-11 15:16:57 +0200

    Initial Socket IO Utils

---
+++ src/utils/socketio.js
@@ -0,0 +1,21 @@
+import io from "socket.io-client";
+import store from "../store/store";
+import { setStreams } from "../store/actions";
+
+const SERVER = "http://localhost:5000";
+
+let socket = null;
+
+const connectToSocketIoServer = () => {
+    socket = io(SERVER);
+
+    socket.on("connect", () => {
+        console.log(`Connected to Socket.IO server: ${socket.id}`);
+    });
+
+    socket.on("all-streams", (streams) => {
+        store.dispatch(setStreams(streams));
+    });
+};
+
+export { connectToSocketIoServer };
